48759bf40b1bf80f54dfbd3cc70b34c3783294a7
KM <tsnok777@gmail.com>
2023-02-28 16:45:07 +0200

    Deleting Posts

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import generics, permissions, mixins,status
+from rest_framework import generics, permissions, mixins, status
 from rest_framework.exceptions import ValidationError
 
 from posts.models import Post, Vote
@@ -13,25 +13,38 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    def perform_create(self,serializer):
-        serializer.save(poster = self.request.user)
+    def perform_create(self, serializer):
+        serializer.save(poster=self.request.user)
 
-class VoteCreate(generics.CreateAPIView,mixins.DestroyModelMixin):
 
+class PostRetriveDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, *args, **kwargs):
+        post = Post.objects.filter(pk=kwargs['pk'], poster=self.request.user)
+        if post.exists():
+            return self.destroy(request,*args,**kwargs)
+        else:
+            raise ValidationError('This post is not deleted. It\'s not your post ' )
+
+
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
     serializer_class = VoteSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
         post = Post.objects.get(pk=self.kwargs['pk'])
-        return Vote.objects.filter(voter = user, post = post)
+        return Vote.objects.filter(voter=user, post=post)
 
-    def perform_create(self,serializer):
+    def perform_create(self, serializer):
         if self.get_queryset().exists():
             raise ValidationError('You have already voted for this post.')
-        serializer.save(voter = self.request.user, post = Post.objects.get(pk=self.kwargs['pk']))
+        serializer.save(voter=self.request.user, post=Post.objects.get(pk=self.kwargs['pk']))
 
-    def delete(self,request,*args,**kwargs):
+    def delete(self, request, *args, **kwargs):
         if self.get_queryset().exists():
             self.get_queryset().delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
