d669bc3244a1e910444a9400852decde2f9b464b
KM <tsnok777@gmail.com>
2023-02-28 14:01:33 +0200

    Deleting Votes

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,5 @@
+from turtle import pos
+
 from rest_framework import serializers
 from .models import Post, Vote
 
@@ -6,10 +8,15 @@ class PostSerializer(serializers.ModelSerializer):
 
     poster = serializers.ReadOnlyField(source='poster.username')
     poster_id = serializers.ReadOnlyField(source='poster.id')
+    votes = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['id','title','url','poster','created','poster_id']
+        fields = ['id','title','url','poster','created','poster_id','votes']
+
+    def get_votes(self,post):
+        return Vote.objects.filter(post=post).count()
+
 
 
 class VoteSerializer(serializers.ModelSerializer):
